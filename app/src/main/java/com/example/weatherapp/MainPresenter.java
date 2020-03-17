cd1d9c8ddc81e741638246e40dae698f4633765e
nandita-appknit <nandita.appknit@gmail.com>
2020-03-17 12:07:27 +0530

    mvp commit

---
+++ app/src/main/java/com/example/weatherapp/MainPresenter.java
@@ -0,0 +1,60 @@
+package com.example.weatherapp;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainPresenter {
+    MainView mainView;
+    public static String BaseUrl = "http://api.openweathermap.org/";
+    public static String API = "04d51d94b6603269028898f672008f50";
+
+    public void display() {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(BaseUrl).addConverterFactory(GsonConverterFactory.create()).build();
+        weatherApi weather = retrofit.create(weatherApi.class);
+        //  String text=cityname.getText().toString().trim();
+        Call<WeatherResponse> call = weather.getCurrentWeatherData(mainView.getcityname(), API);
+        call.enqueue(new Callback<WeatherResponse>() {
+            @Override
+            public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
+                if (response.code() == 200) {
+
+                    WeatherResponse weatherResponse = response.body();
+                    assert weatherResponse != null;
+
+                    String stringBuilder = "Country: " +
+                            weatherResponse.sys.country +
+                            "\n" +
+                            "Temperature: " +
+                            weatherResponse.main.temp +
+                            "\n" +
+                            "Temperature(Min): " +
+                            weatherResponse.main.temp_min +
+                            "\n" +
+                            "Temperature(Max): " +
+                            weatherResponse.main.temp_max +
+                            "\n" +
+                            "Humidity: " +
+                            weatherResponse.main.humidity +
+                            "\n" +
+                            "Pressure: " +
+                            weatherResponse.main.pressure;
+
+                    mainView.updatedText(stringBuilder);
+
+                } else {
+                    mainView.updatedText(response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<WeatherResponse> call, Throwable t) {
+                mainView.updatedText(t.getMessage());
+
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/com/example/weatherapp/MainView.java
@@ -0,0 +1,12 @@
+package com.example.weatherapp;
+
+public interface MainView {
+
+    public String getcityname();
+
+    public void onButtonClicked();
+
+    public void displayText();
+
+    public void updatedText(String string);
+}
